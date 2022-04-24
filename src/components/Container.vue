17166121c597b630205516e77c82b9a31a3cade7
bojun1995 <bojun1995@foxmail.com>
2022-04-24 23:55:21 +0800

    [init]初始化工程

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    startup
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
+.container {
+  height: 100px;
+  width: 300px;
+}
+</style>
