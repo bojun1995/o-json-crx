5c26f483302faa63cdd825dd3fa99d45a6292904
bojun1995 <bojun1995@foxmail.com>
2022-05-09 23:55:08 +0800

    [chg]editor config

--- src/override/App.vue
+++ src/override/App.vue
@@ -6,15 +6,14 @@
 import { onMounted } from 'vue'
 import * as monaco from 'monaco-editor'
 
-self.MonacoEnvironment = {
+window.MonacoEnvironment = {
   getWorker: function (workerId, label) {
     const getWorkerModule = (moduleUrl, label) => {
-      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+      return new Worker(window.MonacoEnvironment.getWorkerUrl(moduleUrl), {
         name: label,
         type: 'module',
       })
     }
-
     switch (label) {
       case 'json':
         return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label)
@@ -34,10 +33,32 @@ self.MonacoEnvironment = {
     }
   },
 }
+
+// {"a": 1, "b": [1, 2, 3]}
+
 onMounted(() => {
   const dom = document.getElementById('layout')
   monaco.editor.create(dom, {
     language: 'json',
+    theme: 'vs-dark',
+    // 可折叠
+    folding: true,
+    // 自适应布局
+    automaticLayout: true,
+    // 行亮
+    renderLineHighlight: 'all',
+    // 显示行号
+    selectOnLineNumbers: true,
+    // 字体大小
+    fontSize: 16,
+    // 取消代码后面一大段空白
+    scrollBeyondLastLine: false,
+    // 不要滚动条的边框
+    overviewRulerBorder: false,
+    // 右上角地图
+    minimap: {
+      enabled: true,
+    },
   })
 })
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import path from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-import { crx } from "rollup-plugin-chrome-extension";
-import zip from "rollup-plugin-zip";
+import { crx } from 'rollup-plugin-chrome-extension'
+import zip from 'rollup-plugin-zip'
 
-import manifest from "./src/manifest.json";
-import pkg from "./package.json";
+import manifest from './src/manifest.json'
+import pkg from './package.json'
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 const crxOptions = {
   manifest: Object.assign(manifest, {
@@ -16,16 +16,16 @@ const crxOptions = {
     version: pkg.version,
     description: pkg.description,
   }),
-};
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": `${path.resolve(__dirname, "src")}/`,
+      '@': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [vue(), crx(crxOptions), isProd && zip({ dir: "releases" })],
+  plugins: [vue(), crx(crxOptions), isProd && zip({ dir: 'releases' })],
   css: {
     preprocessorOptions: {
       scss: {
@@ -36,4 +36,14 @@ export default defineConfig({
       },
     },
   },
-});
+  // 强制预构建插件包
+  optimizeDeps: {
+    include: [
+      `monaco-editor/esm/vs/language/json/json.worker`,
+      `monaco-editor/esm/vs/language/css/css.worker`,
+      `monaco-editor/esm/vs/language/html/html.worker`,
+      `monaco-editor/esm/vs/language/typescript/ts.worker`,
+      `monaco-editor/esm/vs/editor/editor.worker`,
+    ],
+  },
+})
